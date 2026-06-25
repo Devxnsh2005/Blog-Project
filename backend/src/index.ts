a2c11d83223f766aaf2d5d47a669976d6e3ceb3b
Devansh Gupta <devansh190705@gmail.com>
2026-06-25 06:31:56 +0530

    Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import { Hono } from 'hono'
+import { userRouter } from './routes/user'
+import { blogRouter } from './routes/blog'
+import { cors } from 'hono/cors';
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string
+  }
+}>();
+
+app.use(
+  '*',
+  cors({
+    origin: 'http://localhost:5173',
+    allowHeaders: ['Content-Type', 'Authorization'],
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  })
+);
+
+app.route('/api/v1/user', userRouter);
+app.route('/api/v1/blog', blogRouter);
+
+export default app;
